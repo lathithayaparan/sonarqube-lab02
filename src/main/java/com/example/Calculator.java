ee17c9f74936ed41f50e8c696ea4d3930f87783e
lathithayaparan <lathithayaparan.com>
2026-02-01 15:39:55 +0530

    again doing

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -35,4 +35,9 @@ public class Calculator {
     public int sumValues(int a, int b) {
         return a + b;
     }
+    // INTENTIONAL DUPLICATION
+    public int addAgain(int a, int b) {
+         return a + b;
+}
+
 }
